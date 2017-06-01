9c60d330d021ac4238fb04bc9cc260052a0b0309
pix <pix@pix-labs.com>
2017-06-02 00:16:22 +0200

    initial commit

--- js/main.js
+++ js/main.js
@@ -2,10 +2,6 @@
 var saveObject;
 
 $(document).ready(function () {
-    console.log("Loading save from cookie");
-    saveObject = loadSave();
-    console.log("Game loaded");
-    window.setInterval(doAction(saveObject),1000);
 
 });
 
@@ -21,17 +17,71 @@ function loadSave() {
 }
 
 function newGame() {
-    var newGame = {'current' : 0,'capacity':10,'money': 0,'startDate' : new Date()};
+    var newGame = {
+        'version':'0',
+        'power' : '5',
+        'capacity':10,
+        'money': 0,
+        'startDate' : new Date(),
+        'maxPower':10,
+        'materials':{
+            'metal':0,
+            'wood':0,
+            'sand':0,
+            'water':0,
+            'gold':0
+        },
+        'grains':{
+
+        },
+        'world':{
+
+        }};
     return newGame;
 }
 
+function saveGame(saveObject) {
+    $.cookie("pix-labs-savefile",JSON.stringify(saveObject));
+    console.log("game saved!");
+}
 
 function shine() {
     //Make the pixel glow
 }
 
 
-
-function doAction() {
-
+function callAtTimeout() {
+    console.log("Timeout occurred");
 }
+
+function addOneTest(test) {
+    test++;
+    console.log(test);
+}
+
+function checkMaxCurrent(saveObject) {
+    if(saveObject!==undefined && saveObject.hasOwnProperty(power) && saveObject.hasOwnProperty(maxPower)){
+        return current<maxCurrent;
+    }
+}
+
+
+
+angular.module('pixel-game',['ngAnimate'])
+    .controller('gameController',function ($scope,$http,$timeout,$interval) {
+        console.log("Loading save from cookie");
+        $scope.saveObject = loadSave();
+        console.log("Game loaded");
+        $scope.doAction = function () {
+            $scope.saveObject.current++;
+            console.log("Added current");
+        };
+        $scope.save = function() {
+            saveGame($scope.saveObject);
+        };
+        // $timeout(callAtTimeout, 3000);
+        $interval($scope.doAction,1000);
+        $interval($scope.save,10000);
+
+    })
+    ;
